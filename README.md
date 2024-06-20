92285ace9d09d1a027836356e718be8288d18762
Andreas Gajdosik <andreas.gajdosik@gmail.com>
2024-06-20 17:53:06 +0200

    Add some basic structure

--- README.md
+++ README.md
@@ -3,4 +3,5 @@ Library allows to order in browser the asset downloads to the softwares connecte
 
 ## Compile JavaScript
 1. `npm run build`
-
+2. `http-server`
+3. navigate to localhost:8080, on refresh browser tries to fetch data from locally running BlenderKit-client, prints to console
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,46 @@
-const CLIENT_PORTS = ["62485", "65425", "55428", "49452", "35452", "25152", "5152", "1234"]
+const CLIENT_PORTS = ["65425", "55428", "49452", "35452", "25152", "5152", "1234", "62485"];
+let socket: WebSocket | null = null;
+
+window.onload = OnLoad; // Correctly assign the OnLoad function to the window.onload event
 
 
-export function GetConnection(): WebSocket {
-  const socket = new WebSocket("ws://127.0.0.1:" + CLIENT_PORTS[0] + "/");
-  return socket;
+async function ClientIsOnline(): Promise<boolean> {
+    const resp = await GetClientStatus();
+    return resp!== null;
 }
 
+async function GetClientStatus(): Promise<Response|null> {
+    for (const port of CLIENT_PORTS) {
+        const resp = await TryClientStatus(`http://localhost:${port}/bkclientjs/status`)
+        if (resp!== null) {
+            return resp;
+        }
+    }
+    return null;
+}
 
+async function TryClientStatus(url: string): Promise<Response|null> {
+    try {
+        const resp = await fetch(url);
+        if (resp.status === 200) {
+            return resp
+        }
+        console.log(`Bad status code: ${resp.status}`);
 
+    } catch (err) {
+        console.error(err);
+    }
 
+    return null;
+}
 
+async function OnLoad() {
+    const resp = await GetClientStatus();
+    console.log(resp);
+    if (resp === null) {
+        return;
+    }
 
-
-
-
-
-
+    const test = await resp.text();
+    console.log(test);
+}
